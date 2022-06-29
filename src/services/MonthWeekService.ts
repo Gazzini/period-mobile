38260ac8c8c8ae7baa240bf0836406ef2cd01ded
ahood12 <adamghood@gmail.com>
2022-06-28 23:12:21 -0700

    clean

--- src/services/MonthWeekService.ts
+++ src/services/MonthWeekService.ts
@@ -1,8 +1,6 @@
 /// Helper function to get array of weeks in a given month (for displaying a calendar month view)
 export namespace MonthWeekService {
-    // Returns an array of arrays of numbers representing dates in a week.
-    // Uses -1 for an out-of-month date in a week (because null is hard to map over)
-    export const getWeeksForMonthsView = (monthDate: Date): (number)[][] => {
+    export const getWeeks = (monthDate: Date): (number)[][] => {
         const firstDateInMonth = new Date(monthDate.getFullYear(), monthDate.getMonth(), 1);
         const lastDateInMonth = new Date(monthDate.getFullYear(), monthDate.getMonth() + 1, 0);
 
@@ -27,27 +25,6 @@ export namespace MonthWeekService {
             lines[w] = row;
         }
         return lines;
-        // Inspiration from here: https://github.com/krazylek/calendar-month-array/blob/master/index.js
-        // Thanks @krazylek!!
-    };
-
-    // Returns an array of 7 numbers, with the dates from the most recent Sunday -> EOW
-    export const getWeeksForWeekView = (anyDateInWeek: Date): number[] => {
-        const daysSinceSunday = anyDateInWeek.getDay();   // Sunday is 0, Saturday is 6
-
-        const mostRecentSunday = new Date(anyDateInWeek);
-        mostRecentSunday.setDate(
-            anyDateInWeek.getDate() - daysSinceSunday
-        );
-
-        const cursor = mostRecentSunday;
-        const result: number[] = new Array(7);
-        for (let i = 0; i < 7; i++) {
-            result[i] = cursor.getDate();
-            cursor.setDate(cursor.getDate() + 1);
-        }
-
-        return result;
     };
 
     const isInCurrentMonth = (dayOfMonth: number, lastDayOfMonth: number): boolean => {
@@ -59,4 +36,5 @@ export namespace MonthWeekService {
         return new Date(Math.round(date.getTime() / p) * p);
     };
 }
-
+// Inspiration from here: https://github.com/krazylek/calendar-month-array/blob/master/index.js
+// Thanks @krazylek!!
